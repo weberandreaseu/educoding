d6817652adfb01d5060ab3e4caf109f9236ff5d4
Andreas Weber <info@weberandreas.eu>
2016-05-05 16:16:09 +0200

    Create sign up feature

---
+++ features/step_definitions/sign_up_steps.rb
@@ -0,0 +1,15 @@
+When(/^I click sign up$/) do
+  click_link new_user_path
+end
+
+When(/^I fill the registration form$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^submit the form$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+ Then(/^I should get a confirmation$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
