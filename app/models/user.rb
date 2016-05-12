d67333a0f3c66194326a4a4303716a06d822e2a7
Andreas Weber <info@weberandreas.eu>
2016-05-12 16:28:50 +0200

    Implement sign up feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :firstname, presence: true
+
+  validates :lastname, presence: true
+
+  validates :email, format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, uniqueness: true
 end
--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -1,15 +1,19 @@
 When(/^I click sign up$/) do
-  click_link new_user_path
+  click_button "Sign up"
 end
 
 When(/^I fill the registration form$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in "Firstname", with: "John"
+  fill_in "Lastname", with: "Doe"
+  fill_in "Email", with: "john@doe.com"
+  fill_in "Password", with: "1234"
+  fill_in "Password confirmation", with: "1234"
 end
 
 When(/^submit the form$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button "Submit"
 end
 
  Then(/^I should get a confirmation$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+   page.should have_content('successfully registred')
 end
