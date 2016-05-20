bc541ce612262f77c4839ca1fa64f2f7781431f1
Andreas Weber <info@weberandreas.eu>
2016-05-20 11:31:42 +0200

    Description field uses Ace Editor

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -2,3 +2,14 @@
 //= require ace/theme-chrome
 //= require ace/mode-java
 //= require ace/mode-markdown
+
+$(document).ready(function() {
+	var editor = ace.edit("editor");
+	editor.setTheme("ace/theme/chrome");
+	editor.getSession().setMode("ace/mode/markdown");
+
+	$('#new-task').click(function() {
+		var description = editor.getValue();
+		$('#task_description').val(description);
+	});
+});
