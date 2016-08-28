1b1b735ba525e3151ce82e8e1a9cbefbbf7432c2
Andreas Weber <info@weberandreas.eu>
2016-08-28 08:02:14 +0200

    Add downcase email before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :solutions
 
+  before_save :downcase_email
+
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, format: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, uniqueness: true
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
